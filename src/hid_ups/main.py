f695ae4981e2d9da6629e48771bccdc20f41569d
Zen <z@pyl.onl>
2024-01-18 14:26:15 -0600

    fix gathers not being awaited
    Signed-off-by: Zen <z@pyl.onl>

--- src/hid_ups/main.py
+++ src/hid_ups/main.py
@@ -5,6 +5,10 @@ from zenlib.util import init_logger, init_argparser, process_args
 from asyncio import run, gather
 
 
+async def run_loops(loops):
+    await gather(*loops)
+
+
 def main():
     argparser = init_argparser(prog=__package__, description='HID based UPS reader')
     logger = init_logger(__package__)
@@ -18,7 +22,7 @@ def main():
 
     loops = [ups.mainloop() for ups in ups_list]
 
-    run(gather(*loops))
+    run(run_loops(loops))
 
 
 if '__main__' == __name__:
